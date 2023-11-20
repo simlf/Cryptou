c885c77e97ec0db051638bef67dfbbce69e45202
Jibe LELEVIER <devme@MSI.(none)>
2023-11-20 11:31:09 +0100

    ✨ user routes

--- front-cryptou/vite.config.ts
+++ front-cryptou/vite.config.ts
@@ -1,11 +1,30 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     port: 8081,
   },
-})
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: fileURLToPath(new URL("./src", import.meta.url)),
+      },
+      {
+        find: "@components",
+        replacement: fileURLToPath(
+          new URL("./src/components", import.meta.url)
+        ),
+      },
+      {
+        find: "@view",
+        replacement: fileURLToPath(new URL("./src/view", import.meta.url)),
+      },
+    ],
+  },
+});
