bdf5e2bfb241cb0c4b2df1195f442cfdf5b9892a
simlf <simon.le-fourn@epitech.eu>
2023-11-20 11:48:43 +0100

    🗃️ Create DB schema

--- back-cryptou/src/server.ts
+++ back-cryptou/src/server.ts
@@ -1,6 +1,10 @@
+import { PrismaClient } from '@prisma/client'
 const bodyParser = require("body-parser");
 const express = require("express");
+
 const app = express();
+const prisma = new PrismaClient()
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const PORT: string | number = process.env.PORT || 3000;
