af82ab80b102102c359e48472b009379a9a9767b
tistech <contact@matthislesur.com>
2023-11-21 16:38:56 +0100

    :card_file_box: add cryptocurrency seed

--- back-cryptou/prisma/seed.ts
+++ back-cryptou/prisma/seed.ts
@@ -37,6 +37,46 @@ async function main() {
             role: userRole.id,
         },
     });
+
+    // Add Bitcoin to Cryptocurrency
+    await prisma.cryptocurrency.upsert({
+        where: { fullName: 'Bitcoin' },
+        update: {},
+        create: {
+            fullName: 'Bitcoin',
+            slugName: 'btc',
+            imageUrl: 'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579',
+        },
+    });
+
+    await prisma.cryptocurrency.upsert({
+        where: { fullName: 'Ethereum' },
+        update: {},
+        create: {
+            fullName: 'Ethereum',
+            slugName: 'eth',
+            imageUrl: 'https://assets.coingecko.com/coins/images/279/large/ethereum.png?1595348880',
+        },
+    });
+    await prisma.cryptocurrency.upsert({
+        where: { fullName: 'Doge coin' },
+        update: {},
+        create: {
+            fullName: 'Doge coin',
+            slugName: 'doge',
+            imageUrl: 'https://assets.coingecko.com/coins/images/5/large/dogecoin.png?1547792256',
+        },
+    });
+    await prisma.cryptocurrency.upsert({
+        where: { fullName: 'Stable coin' },
+        update: {},
+        create: {
+            fullName: 'Stable coin',
+            slugName: 'usdt',
+            imageUrl: 'https://assets.coingecko.com/coins/images/325/large/Tether-logo.png?1598003707',
+        },
+    });
+
 }
 
 main()
