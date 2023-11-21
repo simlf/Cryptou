9c0842896d895b57fd83f3fda3116a4e076d9e7a
simlf <simon.le-fourn@epitech.eu>
2023-11-21 16:55:49 +0100

    ✨ Add logic to parse and store RSS articles and keywords from feeds

--- back-cryptou/src/rss/rssParser.ts
+++ back-cryptou/src/rss/rssParser.ts
@@ -26,39 +26,48 @@ class RssParser {
     private async storeArticleWithKeywords(feedId: number, item: Parser.Item, keywords: string[]): Promise<void> {
         const articleDate = new Date(item.pubDate || Date.now());
         const imageUrl = this.extractImageUrl(item);
+        console.log(`Aricle ${item.title} - Has this image ${imageUrl}... - ${item}`);
 
         await this.prisma.$transaction(async (prisma) => {
-            const newArticle = await prisma.article.create({
-                data: {
-                    title: item.title || 'Untitled',
-                    summary: item.contentSnippet || item.content || '',
-                    source: item.link || 'Unknown',
-                    date: articleDate,
-                    pageUrl: item.link || '',
-                    imageUrl: imageUrl,
-                },
+            const existingArticle = await prisma.article.findUnique({
+                where: { pageUrl: item.link || '' },
             });
 
-            for (const keywordText of keywords) {
-                let keyword = await prisma.keyword.upsert({
-                    where: {keyword: keywordText},
-                    update: {},
-                    create: {keyword: keywordText},
-                });
-
-                await prisma.articleKeyword.create({
+            if (!existingArticle) {
+                const newArticle = await prisma.article.create({
                     data: {
-                        articleId: newArticle.id,
-                        keywordId: keyword.id,
+                        title: item.title || 'Untitled',
+                        summary: item.contentSnippet || item.content || '',
+                        source: item.link || 'Unknown',
+                        date: articleDate,
+                        pageUrl: item.link || '',
+                        imageUrl: imageUrl,
                     },
                 });
-            }
 
-            // Update the lastArticleDate for the feed
-            await prisma.feed.update({
-                where: {id: feedId},
-                data: {lastArticleDate: articleDate},
-            });
+                for (const keywordText of keywords) {
+                    let keyword = await prisma.keyword.upsert({
+                        where: {keyword: keywordText},
+                        update: {},
+                        create: {keyword: keywordText},
+                    });
+
+                    await prisma.articleKeyword.create({
+                        data: {
+                            articleId: newArticle.id,
+                            keywordId: keyword.id,
+                        },
+                    });
+                }
+
+                // Update the lastArticleDate for the feed
+                await prisma.feed.update({
+                    where: {id: feedId},
+                    data: {lastArticleDate: articleDate},
+                });
+            } else {
+                console.log(`Skipping article ${item.link} - already exists.`);
+            }
         });
     }
 
@@ -72,18 +81,14 @@ class RssParser {
     }
 
     private extractImageUrl(item: Parser.Item): string {
-        // Check the 'media:content' tag
-        const mediaContent = (item as any)['media:content'] as { url: string; type: string } | undefined;
-        if (mediaContent && mediaContent.url && mediaContent.type && mediaContent.type.startsWith('image/')) {
-            return mediaContent.url;
-        }
+        // if (item['media:content'] && item['media:content'].$.url && item['media:content'].$.type.startsWith('image/')) {
+        //     return item['media:content'].$.url;
+        // }
 
-        // Check the 'enclosure' tag
         if (item.enclosure && item.enclosure.url && item.enclosure.type && item.enclosure.type.startsWith('image/')) {
             return item.enclosure.url;
         }
 
-        // Check for images embedded in 'description' content
         const descriptionContent = item.content || item.contentSnippet || '';
         const imgRegex = /<img.*?src=["'](.*?)["']/;
         const imgMatch = descriptionContent.match(imgRegex);
@@ -91,148 +96,7 @@ class RssParser {
             return imgMatch[1];
         }
 
-        // No image found
         return '';
     }
 }
 export default RssParser;
-
-
-// import Parser from 'rss-parser';
-// import { PrismaClient } from '@prisma/client';
-// import keywordExtractor from 'keyword-extractor';
-//
-// class RssParser {
-//     private parser: Parser;
-//     private prisma: PrismaClient;
-//
-//     constructor() {
-//         this.parser = new Parser();
-//         this.prisma = new PrismaClient();
-//     }
-//
-//     public async parseAndStore(feedContent: string): Promise<void> {
-//         const feed = await this.parser.parseString(feedContent);
-//
-//         for (const item of feed.items) {
-//             const keywords = this.extractKeywords(item.contentSnippet || item.content || '');
-//             await this.storeArticleWithKeywords(item, keywords);
-//         }
-//     }
-//
-//     private extractKeywords(content: string): string[] {
-//         return keywordExtractor.extract(content, {
-//             language: "english",
-//             remove_digits: true,
-//             return_changed_case: true,
-//             remove_duplicates: true
-//         });
-//     }
-//
-//     private async storeArticleWithKeywords(feedId: number, item: Parser.Item, keywords: string[]): Promise<void> {
-//         const articleDate = new Date(item.pubDate || Date.now());
-//         const imageUrl = this.extractImageUrl(item);
-//
-//         await this.prisma.$transaction(async (prisma) => {
-//             // Check if the article is newer than the last stored article for the feed
-//             const feed = await prisma.feed.findUnique({
-//                 where: { id: feedId },
-//             });
-//
-//             if (!feed.lastArticleDate || articleDate > feed.lastArticleDate) {
-//                 const newArticle = await prisma.article.create({
-//                     data: {
-//                         title: item.title || 'Untitled',
-//                         summary: item.contentSnippet || item.content || '',
-//                         source: item.link || 'Unknown',
-//                         date: articleDate,
-//                         pageUrl: item.link || '',
-//                         imageUrl: imageUrl,
-//                     },
-//                 });
-//
-//                 for (const keywordText of keywords) {
-//                     let keyword = await prisma.keyword.upsert({
-//                         where: { keyword: keywordText },
-//                         update: {},
-//                         create: { keyword: keywordText },
-//                     });
-//
-//                     await prisma.articleKeyword.create({
-//                         data: {
-//                             articleId: newArticle.id,
-//                             keywordId: keyword.id,
-//                         },
-//                     });
-//                 }
-//
-//                 // Update the lastArticleDate for the feed
-//                 await prisma.feed.update({
-//                     where: { id: feedId },
-//                     data: { lastArticleDate: articleDate },
-//                 });
-//             }
-//         });
-//     }
-//
-//     // private async storeArticleWithKeywords(item: Parser.Item, keywords: string[]): Promise<void> {
-//     //     const imageUrl = this.extractImageUrl(item);
-//     //
-//     //     await this.prisma.$transaction(async (prisma) => {
-//     //         const newArticle = await prisma.article.create({
-//     //             data: {
-//     //                 title: item.title || 'Untitled',
-//     //                 summary: item.contentSnippet || item.content || '',
-//     //                 source: item.link || 'Unknown',
-//     //                 date: new Date(item.pubDate || Date.now()),
-//     //                 pageUrl: item.link || '',
-//     //                 imageUrl: imageUrl,
-//     //             },
-//     //         });
-//     //
-//     //         for (const keywordText of keywords) {
-//     //             let keyword = await prisma.keyword.findUnique({
-//     //                 where: { keyword: keywordText },
-//     //             });
-//     //
-//     //             if (!keyword) {
-//     //                 keyword = await prisma.keyword.create({
-//     //                     data: { keyword: keywordText },
-//     //                 });
-//     //             }
-//     //
-//     //             await prisma.articleKeyword.create({
-//     //                 data: {
-//     //                     articleId: newArticle.id,
-//     //                     keywordId: keyword.id,
-//     //                 },
-//     //             });
-//     //         }
-//     //     });
-//     // }
-//
-//     private extractImageUrl(item: Parser.Item): string {
-//         // Assuming mediaContent is a part of the item but not recognized by TypeScript
-//         const mediaContent = (item as any)['media:content'] as { url: string; type: string } | undefined;
-//         if (mediaContent?.type?.startsWith('image/')) {
-//             return mediaContent.url;
-//         }
-//         // Check enclosure tag
-//         if (item.enclosure?.type?.startsWith('image/')) {
-//             return item.enclosure.url;
-//         }
-//
-//         // Check for images embedded in description/content
-//         const htmlContent = item.content || item.contentSnippet || '';
-//         const imgRegex = /<img.*?src=["'](.*?)["']/;
-//         const imgMatch = htmlContent.match(imgRegex);
-//         if (imgMatch) {
-//             return imgMatch[1];
-//         }
-//
-//         // No image found
-//         return '';
-//     }
-// }
-//
-// export default RssParser;
--- back-cryptou/src/server.ts
+++ back-cryptou/src/server.ts
@@ -1,13 +1,21 @@
-import { PrismaClient } from '@prisma/client'
+import RssFetcher from "./rss/rssFetcher";
+
 const bodyParser = require("body-parser");
 const express = require("express");
-
 const app = express();
-const prisma = new PrismaClient()
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const PORT: string | number = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`server is running on ${PORT}`);
 });
+
+async function startApplication() {
+  console.log('Starting application...')
+  const rssFetcher = new RssFetcher();
+  await rssFetcher.fetchAllFeeds();
+  console.log('Fetch finished application...')
+
+}
+
+startApplication().catch(console.error);
