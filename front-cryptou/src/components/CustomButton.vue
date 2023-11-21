b397b681867e82d4cb14b158e35bdd5aaf851003
tistech <contact@matthislesur.com>
2023-11-21 12:06:27 +0100

    :lipstick: add custom component button text field and wave footer

---
+++ front-cryptou/src/components/CustomButton.vue
@@ -0,0 +1,38 @@
+<template>
+  <button class="custom-button" :style="{backgroundColor: colorBackground}" @click="$emit('onClick')">
+    {{message}}
+  </button>
+</template>
+
+<script setup lang="ts">
+
+defineProps({
+  message: {
+    type: String,
+    required: true
+  },
+  colorBackground: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<style scoped>
+.custom-button {
+  width: 300px;
+  height: 50px;
+  border-radius: 10px;
+  border: none;
+  color: white;
+  font-size: 1.2rem;
+  font-weight: 500;
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+}
+
+.custom-button:hover {
+  transform: scale(1.05);
+}
+
+</style>
---
+++ front-cryptou/src/components/CustomTextField.vue
@@ -0,0 +1,48 @@
+<template>
+  <input class="custom-text-field"
+         :placeholder="placeholder"
+         :style="{backgroundColor: colorBackground}"
+         :value="modelValue"
+         @input="updateValue"/>
+</template>
+
+<script setup lang="ts">
+
+defineProps({
+  placeholder: {
+    type: String,
+    required: true
+  },
+  colorBackground: {
+    type: String,
+    required: true
+  },
+  modelValue: {
+    type: String,
+    required: true
+  }
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+const updateValue = (event) => {
+  emit('update:modelValue', event.target.value)
+}
+</script>
+
+<style scoped>
+.custom-text-field {
+  width: 300px;
+  height: 50px;
+  border-radius: 5px;
+  border: none;
+  font-size: 1.2rem;
+  font-weight: 500;
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+}
+
+.custom-text-field::placeholder {
+  padding-left: 10px;
+}
+</style>
