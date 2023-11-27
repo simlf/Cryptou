dc54d30679a345da2c666a637ca9c2a789e4949f
simlf <simon.le-fourn@epitech.eu>
2023-11-27 10:14:35 +0100

    🚚 Move swagger route

--- back-cryptou/src/server.ts
+++ back-cryptou/src/server.ts
@@ -1,23 +1,17 @@
 import { PrismaClient } from '@prisma/client'
+import swaggerRoutes from './api/swagger';
 const bodyParser = require("body-parser");
 const express = require("express");
 const router = require('express').Router();
-const swaggerUi = require('swagger-ui-express');
-const swaggerJsdoc = require("swagger-jsdoc");
-const swaggerConfig = require('../swagger/swaggerConfig.json');
 const app = express();
 const prisma = new PrismaClient()
 
-const specs = swaggerJsdoc(swaggerConfig);
-
-app.use(
-    "/api-docs",
-    swaggerUi.serve,
-    swaggerUi.setup(specs)
-);
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use(swaggerRoutes);
+
 const PORT: string | number = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`server is running on ${PORT}`);
