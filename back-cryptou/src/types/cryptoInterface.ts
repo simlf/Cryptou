8eb96a213feb5c7748287670299c933fadc51f70
tistech <contact@matthislesur.com>
2023-12-01 18:36:48 +0100

    :truck: add all required route about crypto

---
+++ back-cryptou/src/types/cryptoInterface.ts
@@ -0,0 +1,45 @@
+// types.ts
+
+export interface CryptoChartData {
+    time: number;
+    high: number;
+    low: number;
+    open: number;
+    volumefrom: number;
+    volumeto: number;
+    close: number;
+    conversionType: string;
+    conversionSymbol: string;
+}
+
+export interface CryptoGraphData {
+    Aggregated: boolean;
+    TimeFrom: number;
+    TimeTo: number;
+    Data: CryptoChartData[];
+}
+
+export interface CryptoApiResponse {
+    Response: string;
+    Message: string;
+    HasWarning: boolean;
+    Type: number;
+    RateLimit: object;
+    Data: CryptoGraphData;
+}
+
+export interface CryptoDataSql {
+    id: number;
+    fullName: string;
+    slugName: string;
+    imageUrl: string;
+}
+
+export interface CryptoData {
+    cryptoName: string;
+    currentPrice: number;
+    openingPrice: number;
+    lowestPrice: number;
+    highestPrice: number;
+    imageUrl: string;
+}
