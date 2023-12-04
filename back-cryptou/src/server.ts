055ea33548e930a7c8f3207e9d9a18b6b0cfd6f7
simlf <simon.le-fourn@epitech.eu>
2023-12-04 12:08:00 +0100

    🔧 Handle CORS

--- back-cryptou/src/server.ts
+++ back-cryptou/src/server.ts
@@ -3,6 +3,7 @@ import articlesRoutes from './api/articles';
 import feedsRoutes from './api/feeds';
 const bodyParser = require("body-parser");
 const express = require("express");
+const cors = require("cors");
 const router = require('express').Router();
 import RssFetcher from "./rss/rssFetcher";
 //import userRoutes from "./api/user";
@@ -16,6 +17,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT: string | number = process.env.PORT || 3000;
+
+// Enable All CORS Requests for development purposes
+app.use(cors());
+
+// For production, specify the origin instead of '*'
+// app.use(cors({ origin: 'https://yourfrontenddomain.com' }));
 
 // routes
 //app.use(userRoutes);
