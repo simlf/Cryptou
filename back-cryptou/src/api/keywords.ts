1ba8fc00d20a2c515bf20143dfcdb64653d0afc1
simlf <simon.le-fourn@epitech.eu>
2023-12-05 12:32:20 +0100

    ✨ Return keywords from most occurence

--- back-cryptou/src/api/keywords.ts
+++ back-cryptou/src/api/keywords.ts
@@ -22,48 +22,44 @@ const prisma = new PrismaClient();
  *               items:
  *                 type: object
  *                 properties:
- *                   id:
- *                     type: integer
- *                     description: The unique identifier of the keyword.
- *                   name:
- *                     type: string
- *                     description: The name of the keyword.
- *                   url:
- *                     type: string
- *                     description: A URL to more information about the keyword, possibly to a related search or filter.
- *                   lastFetched:
- *                     type: string
- *                     format: date-time
- *                     description: The timestamp of when the keyword was last fetched from the source.
- *                   lastArticleDate:
- *                     type: string
- *                     format: date-time
- *                     description: The date of the most recent article associated with this keyword.
- *         examples:
- *           application/json:
- *             - id: 1
- *               name: "Blockchain"
- *               url: "https://example.com/keywords/blockchain"
- *               lastFetched: "2023-04-05T08:40:51.620Z"
- *               lastArticleDate: "2023-04-04T11:25:47.200Z"
- *             - id: 2
- *               name: "Bitcoin"
- *               url: "https://example.com/keywords/bitcoin"
- *               lastFetched: "2023-04-05T09:20:33.000Z"
- *               lastArticleDate: "2023-04-05T06:15:29.000Z"
+ *                   keyword:
+ *                    type: string
+ *                    description: The keyword used for filtering articles.
  */
 router.get('/keywords', async (req: Request, res: Response) => {
     try {
-        const keywords = await prisma.keyword.findMany(
-            {
-                select: {
-                    keyword: true,
-                },
+        const keywords = await prisma.keyword.findMany({
+            select: {
+                keyword: true,
+                // Optionally select other fields you need, but exclude the _count
             },
-        );
+            orderBy: {
+                ArticleKeyword: {
+                    _count: 'desc'
+                }
+            }
+        });
+
         res.json(keywords);
     } catch (error: unknown) {
         res.status(500).json({ error: 'An error occurred while fetching keywords' });
     }
 });
+
+// router.get('/keywords', async (req: Request, res: Response) => {
+//     try {
+//         const keywords = await prisma.keyword.findMany(
+//             {
+//                 select: {
+//                     keyword: true,
+//                 },
+//             },
+//         );
+//         res.json(keywords);
+//     } catch (error: unknown) {
+//         res.status(500).json({ error: 'An error occurred while fetching keywords' });
+//     }
+// });
+
+
 export default router;
