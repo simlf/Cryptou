aa572ddab023c12ca5ed9f3c38201e0e4d1e6988
tistech <contact@matthislesur.com>
2023-12-11 17:18:50 +0100

    :wrench: add id to store crypto

--- front-cryptou/src/store/cryptosStore.ts
+++ front-cryptou/src/store/cryptosStore.ts
@@ -11,7 +11,7 @@ export const useCryptoStore = defineStore('cryptos', {
             try {
                 const response = await axios.get('http://localhost:3000/cryptos');
                 // Map over the response data to extract only the fullName of each cryptocurrency
-                this.cryptocurrencyNames = response.data.map((crypto: { fullName: string }) => crypto.fullName);
+                this.cryptocurrencyNames = response.data.map((crypto: { cryptoName: string, id: string }) => ({name: crypto.cryptoName, id: crypto.id}));
             } catch (error) {
                 console.error('There was a problem with the Axios operation:', error);
             }
