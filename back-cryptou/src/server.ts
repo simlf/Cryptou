80747e9d3c01df0aacd41b64d8000f29e0864343
tistech <contact@matthislesur.com>
2023-12-11 10:54:11 +0100

    :truck: fix cors error

--- back-cryptou/src/server.ts
+++ back-cryptou/src/server.ts
@@ -3,17 +3,18 @@ import articlesRoutes from './api/articles';
 import feedsRoutes from './api/feeds';
 const bodyParser = require("body-parser");
 const express = require("express");
-const router = require('express').Router();
 import RssFetcher from "./rss/rssFetcher";
 //import userRoutes from "./api/user";
+import cors from 'cors';
 import cryptoRoutes from "./api/crypto";
-import { PrismaClient } from '@prisma/client';
 
+import { PrismaClient } from '@prisma/client';
 const app = express();
 const prisma = new PrismaClient();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 const PORT: string | number = process.env.PORT || 3000;
 
@@ -24,19 +25,20 @@ app.use(articlesRoutes);
 app.use(feedsRoutes);
 app.use(swaggerRoutes);
 
+app.options('*', cors());
 app.listen(PORT, () => {
-  console.log(`server is running on ${PORT}`);
+    console.log(`server is running on ${PORT}`);
 });
 
 async function startApplication() {
-  console.log('Starting application...')
-  const rssFetcher = new RssFetcher();
-  await rssFetcher.fetchAllFeeds();
-  console.log('Fetch finished application...')
+    console.log('Starting application...')
+    const rssFetcher = new RssFetcher();
+    await rssFetcher.fetchAllFeeds();
+    console.log('Fetch finished application...')
 }
 
 startApplication().catch(console.error);
 
 app.on('close', () => {
-  prisma.$disconnect();
+    prisma.$disconnect();
 });
