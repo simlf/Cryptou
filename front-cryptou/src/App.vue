74bf526b36e8d1a0782d05e597afced8760d7ce4
tistech <contact@matthislesur.com>
2023-12-19 13:55:31 +0100

    :lipstick: dont show navbar on login register page

--- front-cryptou/src/App.vue
+++ front-cryptou/src/App.vue
@@ -1,16 +1,17 @@
 <script setup lang="ts">
-import { RouterView } from "vue-router";
+import { RouterView, useRoute } from "vue-router";
 import NavBarSmall from "../src/components/NavBarSmall.vue";
 import NavBarLarge from "../src/components/NavBarLarge.vue";
 import { useDisplay } from "vuetify";
 
 const { mobile } = useDisplay();
+const route = useRoute();
 </script>
 
 <template>
   <div>
-    <NavBarSmall v-if="mobile" />
-    <NavBarLarge/>
+    <NavBarSmall v-if="mobile && route.name !== 'login' && route.name !== 'register'" />
+    <NavBarLarge v-else-if="route.name !== 'login' && route.name !== 'register'" />
     <router-view />
   </div>
 </template>
